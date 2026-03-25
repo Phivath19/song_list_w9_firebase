7f07664db28448cb446aeb7eeda24dfb00e02019
Phivath19 <maykunnapivath@gmail.com>
2026-03-25 23:51:39 +0700

    first commit

---
+++ lib/Start Code_Song/data/repositories/songs/song_repository_firebase.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../../model/songs/song.dart';
+import '../../dtos/song_dto.dart';
+import 'song_repository.dart';
+
+class SongRepositoryFirebase extends SongRepository {
+  final Uri songsUri = Uri.https(
+    'w9-practice-bf5f0-default-rtdb.asia-southeast1.firebasedatabase.app',
+    '/songs.json',
+  );
+
+  @override
+  Future<List<Song>> fetchSongs() async {
+    final http.Response response = await http.get(songsUri);
+
+    if (response.statusCode == 200) {
+      // 1 - Send the retrieved list of songs
+      Map<String, dynamic> songMap = json.decode(response.body);
+      return songMap.entries
+          .map(
+            (entry) => SongDto.fromJson(
+              entry.key,
+              Map<String, dynamic>.from(entry.value),
+            ),
+          )
+          .toList();
+    } else {
+      // 2- Throw expcetion if any issue
+      throw Exception('Failed to load posts');
+    }
+  }
+
+  @override
+  Future<Song?> fetchSongById(String id) async {}
+}
